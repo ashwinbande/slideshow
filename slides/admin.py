34a4817982495dafa63444dcd3eb2e3047f1e63b
ashwin <ashwin.banty@rediffmail.com>
2020-04-30 14:57:36 +0530

    updated admin and added requirement.txt

--- slides/admin.py
+++ slides/admin.py
@@ -1,7 +1,28 @@
 from django.contrib import admin
 from .models import Presentation, Slide, FeedBack
 
+
 # Register your models here.
-admin.site.register(Presentation)
-admin.site.register(Slide)
-admin.site.register(FeedBack)
+class FeedBackAdmin(admin.ModelAdmin):
+    list_display = ['name', 'mobile', 'email', 'agreement', 'suggestion',
+                    'comment', 'slide', 'presentation']
+    list_filter = ['mobile', 'agreement', 'slide', 'slide__presentation']
+    search_fields = ['name', 'mobile', 'email', ]
+
+    def presentation(self, obj):
+        return obj.slide.presentation
+
+
+class PresentationAdmin(admin.ModelAdmin):
+    list_display = ['name', 'author']
+    list_filter = ['author']
+
+
+class SlideAdmin(admin.ModelAdmin):
+    list_display = ['name', 'presentation']
+    list_filter = ['presentation']
+
+
+admin.site.register(Presentation, PresentationAdmin)
+admin.site.register(Slide, SlideAdmin)
+admin.site.register(FeedBack, FeedBackAdmin)
--- slideshow/settings.py
+++ slideshow/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'rm+o9k&n63s7z$c*jquv0rr!yh7fqquj@yiw7+!y!j@o)r-91u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
